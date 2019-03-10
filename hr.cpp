4fd8d046214c9d085ca3e502514cd0f4584180a6
nefenit <nefenit@gmail.com>
2019-03-10 20:04:08 +0100

    Create hr.cpp

---
+++ hr.cpp
@@ -0,0 +1,34 @@
+/* hr.cpp - writes horizontal bar to terminal
+ * (c) Copyright 2019 Bartosz Mierzynski
+ * Written in C++98
+ */
+
+#include <iostream>
+#include <cstring>
+#include <sys/ioctl.h>
+
+using std::cout;
+using std::endl;
+using std::strlen;
+
+int main(int argc, char *argv[]) {
+	int cols;
+	struct winsize w;
+	
+	ioctl(0, TIOCGWINSZ, &w);
+	cols = w.ws_col > 0 ? w.ws_col : 80;
+	
+	if(argc < 2) {
+		while(cols--)
+			cout.put('#');
+		endl(cout);
+	} else {
+		for(int j = 1; j < argc; j++) {
+			int len= strlen(argv[j]);
+			for(int i = 0; i < cols; i++)
+				cout.put(argv[j][i%len]);
+			endl(cout);
+		}
+	}
+	return 0;
+}
