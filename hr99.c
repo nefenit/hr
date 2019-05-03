93a600fff52ddd72c027abc9a27abd5dd25b2b15
nefenit <nefenit@gmail.com>
2019-05-03 14:47:51 +0200

    Create hr99.c

---
+++ hr99.c
@@ -0,0 +1,31 @@
+/* hr.c - writes horizontal bar to standard output
+ * (c) Copyright 2019 Bartosz Mierzynski
+ * Written in C99
+ */
+ 
+ #include <stdio.h>
+ #include <string.h>
+ #include <sys/ioctl.h>
+ 
+ int main(int argc, char *argv[]) {
+ 	int cols;
+	struct winsize w;
+	
+	ioctl(0, TIOCGWINSZ, &w);
+	cols = w.ws_col > 0 ? w.ws_col : 80;
+	
+	if(argc < 2) {
+		while(cols--)
+			putchar('#');
+		putchar('\n');
+	} else {
+		for(int j = 1; j < argc; ++j) {
+			int len = strlen(argv[j]);
+			for(int i = 0; i < cols; ++i)
+				putchar(argv[j][i%len]);
+			putchar('\n');
+		}
+	}
+	
+ 	return 0;
+}
