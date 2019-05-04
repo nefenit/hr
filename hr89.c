f097d9701c4a335c195a2f4842bf4ec52ceaeb6f
nefenit <nefenit@gmail.com>
2019-05-05 01:38:22 +0200

    Update hr89.c

--- hr89.c
+++ hr89.c
@@ -34,6 +34,7 @@ int main(int argc, char *argv[]) {
  	int i, j, len, cols;
 #ifdef __WIN32__
 	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	
 	if(GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
 		cols = csbi.dwSize.X;
 	else
