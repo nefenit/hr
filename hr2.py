fe9c988178128e3376d685ca3e512d77c1a79295
nefenit <nefenit@gmail.com>
2019-05-04 17:41:52 +0200

    Create hr2.py

---
+++ hr2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python2
+'''
+ hr - horizontal bar for your terminal
+ (c) Copyright 2019 Bartosz Mierzynski
+'''
+
+from sys import argv, exit, stdout
+from fcntl import ioctl
+from struct import pack, unpack
+from termios import TIOCGWINSZ
+
+
+def main():
+	rows, cols, rowsp, colsp = unpack('HHHH', ioctl(0, TIOCGWINSZ, pack('HHHH', 0, 0, 0, 0)))
+        if cols == 0:
+            cols = 80
+
+	if len(argv) < 2:
+		while cols:
+			stdout.write('#');
+			cols -= 1
+		stdout.write('\n');
+	else:
+		for j in range(1, len(argv)):
+			length = len(argv[j])
+			for i in range(0, cols):
+				stdout.write(argv[j][i%length])
+			stdout.write('\n');
+	return 0
+
+if __name__ == "__main__":
+	exit(main())
