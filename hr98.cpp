25c9ebb47b06576f27965df8a985137df26a2199
nefenit <nefenit@gmail.com>
2019-05-12 19:59:36 +0200

    Update hr98.cpp

--- hr98.cpp
+++ hr98.cpp
@@ -7,14 +7,49 @@
 #include <cstring>
 #include <sys/ioctl.h>
 
+#ifdef __WIN32__
+#include <windows.h>
+#else
+#include <sys/ioctl.h>
+#endif /* __WIN32__ */
+
+#ifdef __WIN32__
+  #if WINVER >= _WIN32_WINNT_WINXP  || _WIN32_WINNT >= _WIN32_WINNT_WINXP 
+    #define AMAX 8191
+  #else
+    #define AMAX 2047
+  #endif /* WINVER >= _WIN32_WINNT_WINXP  || _WIN32_WINNT >= _WIN32_WINNT_WINXP */
+#else
+  #ifdef ARG_MAX
+    #define AMAX ARG_MAX
+  #else
+    #ifdef _POSIX_ARG_MAX
+      #define AMAX _POSIX_ARG_MAX
+    #else
+      #define AMAX 4096
+    #endif /* _POSIX_ARG_MAX */
+  #endif /* ARG_MAX */
+#endif /* __WIN32__ */
+
 using std::cout;
 using std::endl;
 using std::strlen;
 
 int main(int argc, char *argv[]) {
+	int cols;
+#ifdef __WIN32__
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	
+	if(GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
+		cols = csbi.dwSize.X;
+	else
+		cols = 80;
+#else
 	winsize w;
+	
 	ioctl(0, TIOCGWINSZ, &w);
-	int cols = w.ws_col > 0 ? w.ws_col : 80;
+	cols = w.ws_col > 0 ? w.ws_col : 80;
+#endif /* __WIN32__ */
 	
 	if(argc < 2) {
 		while(cols--)
