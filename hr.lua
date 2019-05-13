76234df8afbb0a4634fdaa9d6080a0d98d066d06
nefenit <nefenit@gmail.com>
2019-05-13 19:45:59 +0200

    Update hr.lua

--- hr.lua
+++ hr.lua
@@ -6,23 +6,18 @@
 
 local function main()
 	cols = tonumber(os.getenv("COLUMNS"));
-	if(cols == nil)
-	then
+	if cols == nil then
 		cols = 80
 	end
-	if(#arg == 0)
-	then
-		while(cols > 0)
-		do
+	if #arg == 0 then
+		while cols > 0 do
 			io.write('#')
 			cols = cols - 1
 		end
 		io.write('\n')
 	else
-		for j = 1,#arg, 1
-		do
-			for i = 0, cols-1, 1
-			do
+		for j = 1,#arg, 1 do
+			for i = 0, cols-1, 1 do
 				io.write(arg[j]:sub(i%#arg[j]+1,i%#arg[j]+1))
 			end
 			io.write('\n')
@@ -32,4 +27,3 @@ local function main()
 end
 
 os.exit(main())
-
