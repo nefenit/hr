a322ddb7a8ca9c64431d6240191662622a7aa01c
nefenit <nefenit@gmail.com>
2019-05-13 18:19:01 +0200

    Update hr2.py

--- hr2.py
+++ hr2.py
@@ -5,16 +5,37 @@
  (c) Copyright 2019 Bartosz Mierzynski
 '''
 
-from sys import argv, exit, stdout
-from fcntl import ioctl
-from struct import pack, unpack
-from termios import TIOCGWINSZ
+from sys      import argv, exit, stdout
+from struct   import pack, unpack
+from platform import system
 
 __author__    = "Bartosz Mierzynski"
 __copyright__ = "(c) Copyright 2019 Bartosz Mierzynski"
 
 def main():
-	rows, cols, rowspx, colspx = unpack('HHHH', ioctl(0, TIOCGWINSZ, pack('HHHH', 0, 0, 0, 0)))
+        if system() == 'Windows':
+            try:
+                from ctypes import windll, create_string_buffer
+                h    = windll.kernel32.GetStdHandle(-12)
+                csbi = create_string_buffer(22)
+                res  = windll.kernel32.GetConsoleScreenBufferInfo(h, csbi)
+                if res:
+                    (bufx, bufy, curx, cury, wattr,
+                    left, top, right, bottom,
+                    maxx, maxy) = struct.unpack("hhhhHhhhhhh", csbi.raw)
+                    cols = right  - left + 1
+                    rows = bottom - top  + 1
+            except:
+                pass
+        else:
+            try:
+                from fcntl   import ioctl
+                from termios import TIOCGWINSZ
+	        rows, cols, rowspx, colspx \
+	        = unpack('HHHH', ioctl(0, TIOCGWINSZ, pack('HHHH', 0, 0, 0, 0)))
+            except:
+                pass
+
         if cols == 0:
             cols = 80
 
