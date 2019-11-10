7b0bce1cc7e2270bea668a720853b4b2ab2257f6
nefenit <nefenit@gmail.com>
2019-11-10 17:21:41 +0100

    Create hr.rb

---
+++ hr.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+=begin
+/* hr.c - writes horizontal bar to standard output
+ * (c) Copyright 2019 Bartosz Mierzynski
+ * Written in Ruby
+=end
+require 'io/console'
+
+rows, cols = IO.console.winsize	
+if ARGV.length == 0
+  while cols > 0 do
+    print "#";
+    cols -= 1;
+  end
+  puts;
+else
+  for arg in ARGV
+    for i in 0..cols-1
+      print "#{arg[i%arg.length]}";
+    end
+    puts;
+  end
+end
