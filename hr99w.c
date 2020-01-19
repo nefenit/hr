0b2047b4fc5bef3742b8ee8967bcff2413aea316
nefenit <nefenit@gmail.com>
2020-01-19 02:11:03 +0100

    Update hr99w.c

--- hr99w.c
+++ hr99w.c
@@ -1,8 +1,8 @@
 /* hr.c - writes horizontal bar to standard output
- * (c) Copyright 2019 Bartosz Mierzynski
+ * (c) Copyright 2019-2020 Bartosz Mierzynski
  * Written in C99
  */
-
+ 
 #include <limits.h>
 #include <locale.h>
 #include <stdio.h>
@@ -29,44 +29,58 @@
   #endif /* ARG_MAX */
 #endif /* __WIN32__ */
 
-int main(int argc, char *argv[]) {
-	int cols;
+void get_columns(int *n) {
+#ifdef __WIN32__
+	const char                 *columns;
+	CONSOLE_SCREEN_BUFFER_INFO  csbi;
+
+	if((columns = getenv("COLUMNS")) && (strlen(columns)))
+		*n = atoi(columns);
+	else
+		if(GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
+			*n = csbi.dwSize.X;
+		else
+			*n = 80;
+#else
 	const char *columns;
+	struct winsize w;
 
 	if((columns = getenv("COLUMNS")) && (strlen(columns))) {
-		cols = atoi(columns);
+		*n = atoi(columns);
 	} else {
-#ifdef __WIN32__
-		CONSOLE_SCREEN_BUFFER_INFO csbi;
-		
-		if(GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
-			cols = csbi.dwSize.X;
-		else
-			cols = 80;
-#else
-		struct winsize w;
-		
 		ioctl(0, TIOCGWINSZ, &w);
-		cols = w.ws_col > 0 ? w.ws_col : 80;
+		*n = w.ws_col > 0 ? w.ws_col : 80;
+	}
 #endif /* __WIN32__ */
-	}
-	
-	setlocale(LC_ALL, "");
-	
-	if(argc < 2) {
-		while(cols--)
-			putwchar(L'#');
-		putwchar(L'\n');
-	} else {
-		for(int j = 1; j < argc; ++j) {
-			wchar_t arg[AMAX];
-			mbstowcs(arg, argv[j], AMAX);
-			int len = wcslen(arg);
-			for(int i = 0; i < cols; ++i)
-				putwchar(arg[i%len]);
-			putwchar(L'\n');
-		}
-	}
-	
- 	return 0;
 }
+
+void hr1(int n) {
+	while(n--)
+		putwchar(L'#');
+	putwchar(L'\n');
+}
+
+void hr2(int n, const char *s) {
+	wchar_t arg[AMAX];
+	size_t  len;
+
+	mbstowcs(arg, s, AMAX);
+	len = wcslen(arg);
+	for(int i = 0; i < n; ++i)
+		putwchar(arg[i%len]);
+	putwchar(L'\n');
+}
+
+int main(int argc, char *argv[]) {
+	int n;
+
+	setlocale(LC_ALL, "");
+
+	get_columns(&n);
+
+	if(argc < 2)
+		hr1(n);
+	else
+		for(--argc, ++argv; argc; --argc, ++argv)
+			hr2(n, *argv);
+ }
