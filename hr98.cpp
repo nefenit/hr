e9fe9aaa918040c1e201be6044acda32f06606c4
nefenit <nefenit@gmail.com>
2020-01-19 02:30:21 +0100

    Update hr98.cpp

--- hr98.cpp
+++ hr98.cpp
@@ -1,5 +1,5 @@
 /* hr.cpp - writes horizontal bar to terminal
- * (c) Copyright 2019 Bartosz Mierzynski
+ * (c) Copyright 2019-2020 Bartosz Mierzynski
  * Written in C++98
  */
 
@@ -16,38 +16,52 @@ using std::cout;
 using std::endl;
 using std::strlen;
 
-int main(int argc, char *argv[]) {
-	int cols;
+void get_columns(int &n) {
+#ifdef __WIN32__
+	const char                 *columns;
+	CONSOLE_SCREEN_BUFFER_INFO  csbi;
+
+	if((columns = getenv("COLUMNS")) && (strlen(columns)))
+		n = atoi(columns);
+	else
+		if(GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
+			n = csbi.dwSize.X;
+		else
+			n = 80;
+#else
 	const char *columns;
+	struct winsize w;
 
 	if((columns = getenv("COLUMNS")) && (strlen(columns))) {
-		cols = atoi(columns);
+		n = atoi(columns);
 	} else {
-#ifdef __WIN32__
-		CONSOLE_SCREEN_BUFFER_INFO csbi;
-		
-		if(GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
-			cols = csbi.dwSize.X;
-		else
-			cols = 80;
-#else
-		winsize w;
-		
 		ioctl(0, TIOCGWINSZ, &w);
-		cols = w.ws_col > 0 ? w.ws_col : 80;
+		n = w.ws_col > 0 ? w.ws_col : 80;
+	}
 #endif /* __WIN32__ */
-	}
+}
+
+void hr1(int n = 80) {
+	while(n--)
+		cout.put('#');
+	endl(cout);
+}
+
+void hr2(int n = 80, const char *s = "#") {
+	int len = strlen(s);
+	for(int i = 0; i < n; ++i)
+		cout.put(s[i%len]);
+	endl(cout);
+}
+
+int main(int argc, char *argv[]) {
+	int n;
+
+	get_columns(n);
 	
-	if(argc < 2) {
-		while(cols--)
-			cout.put('#');
-		endl(cout);
-	} else {
-		for(int j = 1; j < argc; ++j) {
-			int len = strlen(argv[j]);
-			for(int i = 0; i < cols; ++i)
-				cout.put(argv[j][i%len]);
-			endl(cout);
-		}
-	}
+	if(argc < 2)
+		hr1(n);
+	else
+		for(--argc, ++argv; argc; --argc, ++argv)
+			hr2(n, *argv);
 }
