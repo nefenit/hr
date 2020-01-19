5e063698126ca2076630d80a4ad8b337796d9bc7
nefenit <nefenit@gmail.com>
2020-01-19 01:23:11 +0100

    Refactored Python 3 version

--- hr3.py
+++ hr3.py
@@ -2,37 +2,50 @@
 # -*- coding: utf-8 -*-
 '''
  hr - horizontal bar for your terminal
- (c) Copyright 2019 Bartosz Mierzynski
+ (c) Copyright 2019-2020 Bartosz Mierzynski
 '''
 
 __author__    = "Bartosz Mierzynski"
-__copyright__ = "(c) Copyright 2019 Bartosz Mierzynski"
+__copyright__ = "(c) Copyright 2019-2020 Bartosz Mierzynski"
+
+def get_columns():
+    from os import getenv 
+    n = getenv("COLUMNS")
+    if n == None or n == "":
+        try:
+            from os import get_terminal_size
+            n = get_terminal_size().columns
+        except OSError:
+            n = 80
+    else:
+        n = int(n)
+    return n
+
+def hr1(n = 80):
+    from sys import stdout
+    while n:
+        stdout.write('#');
+        n -= 1
+    stdout.write('\n');
+
+def hr2(n = 80, s = "#"):
+    from sys import stdout
+    length = len(s)
+    for i in range(0, n):
+        stdout.write(s[i % length])
+    stdout.write('\n');
 
 def main():
-        from sys import argv, stdout
-        from os  import getenv 
-        cols = getenv("COLUMNS")
-        if cols == None or cols == "":
-                try:
-                        from os import get_terminal_size
-                        cols = get_terminal_size().columns
-                except OSError:
-                        cols = 80
-        else:
-                cols = int(cols)
-        if len(argv) < 2:
-                while cols:
-                        stdout.write('#');
-                        cols -= 1
-                stdout.write('\n');
-        else:
-                for j in range(1, len(argv)):
-                        length = len(argv[j])
-                        for i in range(0, cols):
-                                stdout.write(argv[j][i%length])
-                        stdout.write('\n');
-        return 0
+    from sys import argv
+    argc = len(argv)
+    n    = get_columns()
+    if argc < 2:
+        hr1(n)
+    else:
+        for i in range(1, argc):
+            hr2(n, argv[i])
+    #from sys import exit
+    #exit(0)
 
 if __name__ == "__main__":
-        from sys import exit
-        exit(main())
+    main()
